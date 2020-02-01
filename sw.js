db8a726c4ec30f38b4db836f5c845d3fbeb6db52
fukubaka0825 <fukubaka0825@users.noreply.github.com>
2020-02-01 12:09:10 +0000

    Automated deployment: Sat Feb  1 12:09:10 UTC 2020 8115df0fb6328401f1bd66d1db02ad93f1aed6de

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-147b57c5b0d733269d3e.js"
+    "url": "webpack-runtime-02d302bfc642bc26e667.js"
   },
   {
     "url": "commons-e4d1cf937e8afc4814a6.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "118b517527874c75befba600c8f42e02"
+    "revision": "ad600b92632d72297137255ae59a59bf"
   },
   {
     "url": "manifest.webmanifest",
