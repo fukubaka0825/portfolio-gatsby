a3eb023645facdeb78f4dc23bac31c3919aeec05
fukubaka0825 <st5818129@gmail.com>
2021-11-27 20:00:17 +0900

    fix certificatiokn

--- src/components/Certification.tsx
+++ src/components/Certification.tsx
@@ -16,13 +16,13 @@ const Certification: React.FC<CertificationProps> = ({ }) => {
                     <h3>2019-03 AWS Certified Solutions Architect Associate</h3>
                 </li>
                 <li>
-                    <h3>2018-11 基本情報技術者</h3>
+                    <h3>2018-11 Fundamental Information Technology Engineer Examination(基本情報技術者)</h3>
                 </li>
                 <li>
                     <h3>2018-04 TOEIC 850</h3>
                 </li>
                 <li>
-                    <h3>2016-04 日商簿記3級</h3>
+                    <h3>2016-04 The Official Business Skill Test in Book-keeping,3rd grade(日商簿記3級)</h3>
                 </li>
             </ul>
         </section>
