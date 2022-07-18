95d2e1a7c6c02310801124e4e333577f50322c5e
fukubaka0825 <fukubaka0825@users.noreply.github.com>
2022-07-18 09:47:00 +0000

    Automated deployment: Mon Jul 18 09:47:00 UTC 2022 72a630c2aba0f3a7083ad33bf7d328df91ef9627

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "styles.eb8e3026dc2695b7a6ec.css"
   },
   {
-    "url": "styles-2d8b9a4a5eabc9f3a57f.js"
+    "url": "styles-811605580d370474cec4.js"
   },
   {
     "url": "commons-c85dc5e21fd4026bb94a.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "33a72dfb64cf6278be2312c48292da81"
+    "revision": "6ca49bb63485441058961c20afa4bedf"
   },
   {
     "url": "manifest.webmanifest",
