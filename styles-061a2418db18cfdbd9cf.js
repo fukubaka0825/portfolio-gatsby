7e9b7303481a9c5b761926a7a56348e7e4ad8fd7
fukubaka0825 <fukubaka0825@users.noreply.github.com>
2025-05-21 20:14:37 +0000

    Automated deployment: Wed May 21 20:14:37 UTC 2025 d5013c5d40cef79bd7f7c07eda16de6f16e928b6

--- styles-061a2418db18cfdbd9cf.js
+++ styles-061a2418db18cfdbd9cf.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[1],{194:function(n,o,w){},226:function(n,o,w){}}]);
-//# sourceMappingURL=styles-95b042fbb6bcdb152026.js.map
+//# sourceMappingURL=styles-061a2418db18cfdbd9cf.js.map
--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "styles.3a91b9ac917573ebfb07.css"
   },
   {
-    "url": "styles-95b042fbb6bcdb152026.js"
+    "url": "styles-061a2418db18cfdbd9cf.js"
   },
   {
     "url": "commons-1bf109e2dcc8e46a5bcc.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1a2fce7ac0d5ea54ae6f59105b987b70"
+    "revision": "f384d7fccc2d3a8532d4631a2d69fdc8"
   },
   {
     "url": "manifest.webmanifest",
